f539d26e428044c41a8f410758c1c1ebd43e2d98
Алексей <minus.odin1@gmail.com>
2015-03-24 15:47:48 +0300

    Мелкие изменения

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ import Tests.Results;
 
 public class Main {
     public static void main(String[] args) {
-        Results testResults = new Results(20, 4, "Купрейчик Алексей");
-        System.out.println("Ответы на тест:");
+        Results testResults = new Results(20, 4, "Фамилия Имя");
         testResults.representAnswers();
     }
 }
